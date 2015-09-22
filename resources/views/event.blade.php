1d98f265fb4aea1206c90bc1a2a6165ec795c654
marktuom <markus.tuominen@live.fi>
2015-09-22 20:51:44 +0900

    Eventin näkymän uudistus

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -4,17 +4,30 @@
 @section('title', 'Aktiviteetti')
 @section('content')
 
-<html>
-<body>
-<h1>{{ $event->name }}</h1>
-<hr>
-<h2></h2>
-<ul>
-    <li><strong>Paikka: </strong>{{ $event->place }}</li>
-    <li><strong>Aika: </strong>{{ $event->time->toDateTimeString() }}</li>
-    <li><strong>Kuvaus: </strong>{{ $event->description }}</li>
-</ul>
+<div class="container">
+    <h1>{{ $event->name }}</h1>
+    <hr>
+    <div class="panel panel-default">
+        <div class="panel-heading"><strong>Tiedot:</strong></div>
+        <div class="panel-body">
+            <ul>
+                <li><strong>Päivä: </strong>{{ $event->time->format('d.m.Y') }}</li>
+                <li><strong>Aika: </strong>{{ $event->time->format('H:i') }}</li>
+                <li><strong>Paikka: </strong>{{ $event->place }}</li>
+            </ul>
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-heading"><strong>Kuvaus:</strong></div>
+        <div class="panel-body">
+            {{ $event->description }}
+        </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-heading"><strong>Aktiviteetit:</strong></div>
+        <div class="panel-body">
+        </div>
+    </div>
+</div>
 
-</body>
-</html>
 @endsection
