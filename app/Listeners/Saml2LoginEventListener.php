5541e3b5afe5515295af89bcaa46d7691306b57b
marktuom <markus.tuominen@live.fi>
2015-09-23 02:09:19 +0900

    laravel saml 2 eventit ja middleware

---
+++ app/Listeners/Saml2LoginEventListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\Aacotroneo\Saml2\Events\Saml2LoginEvent;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\User;
+
+class Saml2LoginEventListener {
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct() {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Saml2LoginEvent  $event
+     * @return void
+     */
+    public function handle(Saml2LoginEvent $event) {
+        $user = $event->getSaml2User();
+        $userData = [
+            'id' => $user->getUserId(),
+            'attributes' => $user->getAttributes(),
+            'assertion' => $user->getRawSamlAssertion()
+        ];
+        $laravelUser = User::firstOrCreate(['partio_id' => $userData->id]);
+        Auth::login($laravelUser);
+    }
+
+}
---
+++ app/Listeners/Saml2LogoutEventListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\Aacotroneo\Saml2\Events\Saml2LogoutEvent;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Saml2LogoutEventListener {
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct() {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  Saml2LogoutEvent  $event
+     * @return void
+     */
+    public function handle(Saml2LogoutEvent $event) {
+        Auth::logout();
+        Session::save();
+    }
+
+}
