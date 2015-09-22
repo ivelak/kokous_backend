a8467a550eb7133d5e644a657d3215c4fce7070f
marktuom <markus.tuominen@live.fi>
2015-09-22 18:53:32 +0900

    Reitityksen ryhmittely ja Rest api

--- app/Http/Controllers/EventRestController.php
+++ app/Http/Controllers/EventRestController.php
@@ -3,72 +3,59 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Event;
-use Carbon\Carbon;
 
-class EventController extends Controller
+class EventRestController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
-        $events = Event::all();
-        
-        return view('events', compact('events'));
+        return Event::all();
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return Response
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        return view('newEvent');
+        //
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  Request  $request
-     * @return Response
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        $event = new Event();
-        $event->name = $request->input('name');
-        $event->time = Carbon::createFromFormat('d/m/Y H:i',$request->input('date').' '.$request->input('time')); 
-        $event->place = $request->input('place');
-        $event->description = $request->input('description');
-        $event->save();
-        
-        return redirect('events');
+        //
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return Response
+     * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        $event = Event::findOrFail($id);
-        return view('event', ['event' => $event]);
+        return Event::findOrFail($id);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return Response
+     * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
@@ -78,9 +65,9 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  Request  $request
+     * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return Response
+     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
@@ -91,7 +78,7 @@ class EventController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return Response
+     * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,24 +1,36 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
 
 
 
 
-Route::get('/', "EventController@create");
-Route::get('/activities', "ActivityController@index");
-Route::get('/activities/new', "ActivityController@create");
-Route::get('/events', "EventController@index");
-Route::get('/events/new', "EventController@create");
-Route::get('/events/{id}', "EventController@show");
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::post('/events/new',"EventController@store");
+
+Route::group(['prefix' => 'events'], function () {
+    Route::get('/', "EventController@index");
+    Route::get('/{id}', "EventController@show")->where('id', '[0-9]+');
+    Route::get('/new', "EventController@create");
+    Route::post('/new', "EventController@store");
+});
+
+Route::group(['prefix' => 'activities'], function () {
+    Route::get('/', "ActivityController@index");
+    Route::get('/new', "ActivityController@create");
+});
+
+Route::group(['prefix' => 'api/dev'], function () {
+    Route::resource('events', 'EventRestController',  ['only' => ['index', 'show']]);
+});
