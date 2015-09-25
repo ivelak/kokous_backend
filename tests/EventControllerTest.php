a0e08b2121c1f757488dd5ede2a1af6a31dc11a1
Mikael Parvamo <mikael.parvamo@gmail.com>
2015-09-25 22:29:18 +0300

    EventController testejä lisätty, EventsView testejä tehty ja NewEventViewn testejä myöskin kirjoitettu.

--- tests/EventControllerTest.php
+++ tests/EventControllerTest.php
@@ -19,7 +19,7 @@ class EventControllerTest extends TestCase
         //$this->seeInDatabase('events',['name'=>'Marko']);
         
         $this->visit('/events/new')
-             ->type('Marko','name')
+             ->type('Hippa','name')
              ->type('27.03.2016','date')
              ->type('23:45','time')
              ->type('Onkalo','place')
@@ -27,7 +27,7 @@ class EventControllerTest extends TestCase
              ->press('Lisää tapahtuma')
              ->seePageIs('/events')
         
-             ->seeInDatabase('events', ['name'=>'Marko','place'=>'Onkalo', 'description'=>'Kuvaus']);
+             ->seeInDatabase('events', ['name'=>'Hippa','time'=>'2016-03-27 23:45:00','place'=>'Onkalo', 'description'=>'Kuvaus']);
            
         
     }
@@ -43,8 +43,38 @@ class EventControllerTest extends TestCase
                 
              ->seePageIs('/events/new')
         
-             ->notSeeInDatabase('events', ['place'=>'Kukkula', 'description'=>'Kuvaus']);
+             ->notSeeInDatabase('events', ['place'=>'Kukkula','time'=>'2016-04-30 23:40:00', 'description'=>'Kuvaus']);
              
     }
+    
+    public function testEventIsNotCreatedWithPastDate(){
+        $this->visit('/events/new')
+             ->type('Jumppa','name')
+             ->type('15.07.2014','date')
+             ->type('21:43','time')
+             ->type('Helsinki','place')
+             ->type('Kuvaus','description')
+             ->press('Lisää tapahtuma')
+                
+             ->seePageIs('/events/new')
+        
+             ->notSeeInDatabase('events', ['name'=>'Jumppa','place'=>'Helsinki','time'=>'2014-07-15 21:43:00', 'description'=>'Kuvaus']);
+    }
+    
+    public function testEventIsCreatedWithoutDescription(){
+        $this->visit('/events/new')
+             ->type('Ilmakitaraturnaus','name')
+             ->type('25.06.2018','date')
+             ->type('16:20','time')
+             ->type('Helsinki','place')
+             ->type('','description')
+             ->press('Lisää tapahtuma')
+                
+             ->seePageIs('/events')
+        
+             ->SeeInDatabase('events', ['name'=>'Ilmakitaraturnaus','place'=>'Helsinki','time'=>'2018-06-25 16:20:00', 'description'=>'']);
+    }
+    
+    
     
 }
---
+++ tests/NewEventViewTest.php
@@ -0,0 +1,104 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class NewEventViewTest extends TestCase {
+
+    use DatabaseMigrations;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testRedirectsToCorrectViewWhenValidInput() {
+        $this->visit('/events/new')
+                ->type('Kokous', 'name')
+                ->type('27.06.2018', 'date')
+                ->type('16:30', 'time')
+                ->type('Helsinki', 'place')
+                ->type('Iltakokous', 'description')
+                ->press('Lisää tapahtuma')
+                
+                ->seePageIs('/events'); //Ohjautuu tapahtumien listausnäkymään
+    }
+    
+    public function testShowsErrorMessageWhenInvalidTimeInput(){
+        $this->visit('/events/new')
+                ->type('Kokous', 'name')
+                ->type('27.06.2014', 'date')
+                ->type('16:45', 'time')
+                ->type('Kamppi', 'place')
+                ->type('Iltakokous', 'description')
+                ->press('Lisää tapahtuma')
+                
+                ->seePageIs('/events/new')
+                ->see('The date must be a date after'); //Näyttää virheviestin liittyen menneeseen aikaan
+    }
+    
+    public function testReturnsToCorrectViewWhenCancelIsPressed(){
+        $this->visit('/events/new')
+                ->type('Kokous', 'name')
+                ->type('23.08.2016', 'date')
+                ->type('16:45', 'time')
+                ->type('Pukinmäki', 'place')
+                ->type('Iltakokous', 'description')
+                
+                ->click('Peruuta')
+                ->seePageIs('/events'); //Palaa listausnäkymään
+    }
+    
+    public function testShowsErrorMessageWhenNameIsNotEntered(){
+        $this->visit('/events/new')
+                ->type('', 'name')
+                ->type('14.05.2016', 'date')
+                ->type('18:30', 'time')
+                ->type('Helsinki', 'place')
+                ->type('Kokous', 'description')
+                ->press('Lisää tapahtuma')
+                
+                ->seePageIs('/events/new')
+                ->see('The name field is required'); //Virheviesti nimen puuttumisesta
+    }
+    
+    public function testShowsErrorMessageWhenPlaceIsNotEntered(){
+        $this->visit('/events/new')
+                ->type('Tanssi', 'name')
+                ->type('25.05.2017', 'date')
+                ->type('20:30', 'time')
+                ->type('', 'place')
+                ->type('Partiotanssit', 'description')
+                ->press('Lisää tapahtuma')
+                
+                ->seePageIs('/events/new')
+                ->see('The place field is required'); //Virheviesti paikan puuttumisesta
+    }
+    
+    public function testShowsErrorMessageWhenTimeIsNotInValidFormat(){
+        $this->visit('/events/new')
+                ->type('Pyöräily', 'name')
+                ->type('25.06.2016', 'date')
+                ->type('10.40', 'time')
+                ->type('Konala', 'place')
+                ->type('Pyöräilyretki', 'description')
+                ->press('Lisää tapahtuma')
+                
+                ->seePageIs('/events/new')
+                ->see('The time does not match the format H:i'); //Virheviesti ajan väärästä formaatista
+    }
+    
+    public function testShowsErrorMessageWhenDateIsNotInValidFormat(){
+        $this->visit('/events/new')
+                ->type('Melonta', 'name')
+                ->type('25-07-2016', 'date')
+                ->type('16:40', 'time')
+                ->type('Munkkiniemi', 'place')
+                ->type('Melontaretki', 'description')
+                ->press('Lisää tapahtuma')
+                
+                ->seePageIs('/events/new')
+                ->see('The date does not match the format d.m.Y'); //Virheviesti päivämäärän väärästä formaatista
+    }
+
+}
