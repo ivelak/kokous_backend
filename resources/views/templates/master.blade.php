27f2a6cc87ca9d3f27b9f0dbfa40e41768d580a6
marktuom <markus.tuominen@live.fi>
2015-09-25 21:23:01 +0900

    Importit saattavat toimia https

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -8,11 +8,11 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        {!!Html::style('css/bootstrap.min.css')!!}
-        {!!Html::style('css/bootstrap-theme.min.css')!!}
+        {!!Html::style('css/bootstrap.min.css', [], Request::secure())!!}
+        {!!Html::style('css/bootstrap-theme.min.css', [], Request::secure())!!}
         @yield('styles')     
-        {!! Html::script('js/jquery-2.1.4.js') !!}
-        {!! Html::script('js/bootstrap.min.js') !!}
+        {!! Html::script('js/jquery-2.1.4.js', [], Request::secure()) !!}
+        {!! Html::script('js/bootstrap.min.js', [], Request::secure()) !!}
         @yield('scripts')
         <title>@yield('title')</title>
     </head>
