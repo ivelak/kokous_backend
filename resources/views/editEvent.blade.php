eb8301425cde32f1004861c0ad4d00702c08f129
Leo Pynnonen <leo.pynnonen@helsinki.fi>
2015-10-12 15:27:38 +0300

    Tapahtuman muokkaukseen lisätty toisto

--- resources/views/editEvent.blade.php
+++ resources/views/editEvent.blade.php
@@ -11,7 +11,7 @@
     @if( isset($errors))
     <ul class="list-group">
         @foreach($errors->all() as $error)
-             <li class="list-group-item list-group-item-danger">{{$error}}</li>
+        <li class="list-group-item list-group-item-danger">{{$error}}</li>
         @endforeach
     </ul>
     @endif
@@ -31,7 +31,39 @@
         <div class="form-group">
             {!!Form::label('time', 'Aika:')!!}<br/>
             {!!Form::text('time', $event->time->format('H:i'), ['class'=>'form-control', 'placeholder'=>'hh:mm'])!!}
+        </div>' <div class="form-group">
+            {!!Form::label('repeat', 'Toisto:')!!}
+            {!!Form::checkbox('repeat', old('repeat'), ['class'=>'form-control'])!!}<br/>
         </div>
+
+        <div class="form-group">
+
+            @foreach (array( 0 => 'Ma', 1 => 'Ti', 2 => 'Ke', 
+            3 => 'To', 4 => 'Pe', 5 => 'La', 6 => 'Su' )as $i => $weekday)
+
+            {!!Form::label($weekday, $weekday)!!}
+            <input type="checkbox" name="days[]" value="{{$i}}" id="{{$weekday}}" class="checkbox-inline"/>
+            &emsp;
+            @endforeach          
+        </div>
+
+        <div class="form-group">
+            {!!Form::label('interval', 'Toistoväli:')!!}           
+            {!!Form::select('interval', array('1','2','3','4'), ['class'=>'form-control'])!!}
+            {!!Form::label('interval', 'viikon välein.')!!}   
+        </div>
+
+        <div class="form-group">
+            {!!Form::label('ending', 'Päättyy:')!!}<br/>
+            <input type="radio" name="ending" value="never" checked> Ei koskaan
+
+            <div class="input-group">
+
+                <span class=""><input type="radio" name="ending" value="until"> Päivänä </span>
+                {!!Form::text('endDate', old('endDate'), ['class'=>'form-control', 'placeholder'=>'dd.mm.yyyy'])!!}
+            </div>
+        </div>
+
 
         <div class="form-group">
             {!!Form::label('place', 'Paikka:')!!}<br/>
