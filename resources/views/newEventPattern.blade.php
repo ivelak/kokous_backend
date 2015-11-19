2f9a4d97933afb63ce771d79ba95fade733aacfb
marktuom <markus.tuominen@live.fi>
2015-11-19 04:50:39 +0200

    Tapahtumapohja lomakkeen muokkausta

--- resources/views/newEventPattern.blade.php
+++ resources/views/newEventPattern.blade.php
@@ -25,22 +25,22 @@
         </div>
         
         <div class="form-group">
-            <input id="radio1" type="radio" name="mode1" value="afterYear" checked> Tapahtuu yhtenä päivänä
+            <input id="radio1" type="radio" name="dateType" checked> Tapahtuu yhtenä päivänä
             <br/>
-            <input id="radio2" type="radio" name="mode2" value="afterYear"> Tapahtuu aikavälillä
+            <input id="radio2" type="radio" name="dateType"> Tapahtuu aikavälillä
         </div>
         
 
         <div class="form-group">
             {!!Form::label('name', 'Tapahtuma päivä:')!!}<br/>
-            {!!Form::text('date', old('date'), ['class'=>'form-control', 'placeholder'=>'dd.mm', 'id'=>'date'])!!}
+            {!!Form::text('date', old('date'), ['class'=>'form-control', 'placeholder'=>'dd.mm.yyyy', 'id'=>'date'])!!}
         </div>
         
         {!!Form::label('name', 'Aikaväli:')!!}
         <div class="input-group">
-            <input id="field1" type="text" class="form-control" placeholder="Alkamispäivä dd.mm" disabled>
+            <input id="field1" name="start" type="text" class="form-control" placeholder="Alkamispäivä dd.mm.yyyy" disabled>
             <span class="input-group-addon">-</span>
-            <input id="field2" type="text" class="form-control" placeholder="Loppumispäivä dd.mm" disabled>
+            <input id="field2" name="end" type="text" class="form-control" placeholder="Loppumispäivä dd.mm.yyyy" disabled>
         </div>
         
         <br>
@@ -50,7 +50,7 @@
             {!!Form::label('activities', 'Valitse aktiviteetit:')!!}
             <select id="selector" multiple="multiple" class="form-control sieve" name="activities[]" id="activities">
                 @forelse($activities as $activity)
-                <option id="{{ $activity->id }}" value="{{ $activity->id }}">{{ $activity->name }}</option>             
+                <option value="{{ $activity->id }}">{{ $activity->name }}</option>             
                 @empty
                 <option disabled value="">Ei aktiviteetteja</option>
                 @endforelse
@@ -65,37 +65,52 @@
             <ul class="list-group" id="activityList">
             </ul>
         </div>
-        
-
+        <hr/>
+         {!!Form::submit('Luo tapahtumapohja', ['class' => 'btn btn-primary'])!!}
     </div>
 </div>
 
 <script>
     
+//    function addActivitiesToList()
+//    {
+//        var nodes = document.getElementById('selector');
+//        for(i=0; i<nodes.options.length; i++) {
+//            var node = nodes.options[i];
+//            if(node.selected === true)
+//            {
+//                var activityList = document.getElementById('activityList');
+//                node.disabled = true;
+//                node.selected = false;
+//                activityList.insertAdjacentHTML('beforeend', '<li class="list-group-item" name=' + node.value + '>'+ node.text +'<span class="pull-right"><button class="btn btn-xs btn-warning" onclick="removeActivity(' + node.value +')" type="button"><span class="glyphicon glyphicon-trash"></span></button></span></li>');
+//            }
+//        }
+//    }
+//    function select_option(i) {
+//        return $('span#span_id select option[value="' + i + '"]').html();
+//    }
+//    function removeActivity(activity) {
+//        var node = document.getElementById(activity);
+//        node.disabled = false;
+//        node.selected = false;
+//        return (elem=document.getElementsByName(activity)[0]).parentNode.removeChild(elem);
+//    }
+//    
+//JQueried verions
     function addActivitiesToList()
     {
-        var nodes = document.getElementById('selector');
-        for(i=0; i<nodes.options.length; i++) {
-            var node = nodes.options[i];
-            if(node.selected === true)
-            {
-                var activityList = document.getElementById('activityList');
-                node.disabled = true;
-                node.selected = false;
-                activityList.insertAdjacentHTML('beforeend', '<li class="list-group-item" name=' + node.value + '>'+ node.text +'<span class="pull-right"><button class="btn btn-xs btn-warning" onclick="removeActivity(' + node.value +')" type="button"><span class="glyphicon glyphicon-trash"></span></button></span></li>');
-            }
-        }
-    }
-    
-    function select_option(i) {
-        return $('span#span_id select option[value="' + i + '"]').html();
+        $('#selector option:selected').each(function () {
+            $(this).removeAttr("selected");
+            $(this).attr('disabled', true);
+            $('<li class="list-group-item" id=' + $(this).val() + '>'+ $(this).html() +'<span class="pull-right"><button class="btn btn-xs btn-warning" onclick="removeActivity(' + $(this).val() +')" type="button"><span class="glyphicon glyphicon-trash"></span></button></span></li>').appendTo('#activityList');
+        });
     }
     
     function removeActivity(activity) {
-        var node = document.getElementById(activity);
-        node.disabled = false;
-        node.selected = false;
-        return (elem=document.getElementsByName(activity)[0]).parentNode.removeChild(elem);
+        var elem = $("[value=" + activity + "]");
+        elem.removeAttr("disabled selected");
+        console.log("li #"+ activity);
+        $("#"+ activity).remove();
     }
     
     $("#radio2").change(function () {
