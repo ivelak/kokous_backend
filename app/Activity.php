b2711feec11ce749c725fe69c450de87b5dacf89
VoxWave <victor_bankowski@hotmail.com>
2015-12-05 22:29:12 +0200

    forgot to add one additional thing to fix the error

--- app/Activity.php
+++ app/Activity.php
@@ -11,7 +11,7 @@ class Activity extends Model
      *
      * @var array
      */
-    protected $fillable = ['name', 'guid', 'age_group', 'task_group', 'logo'];
+    protected $fillable = ['name', 'guid', 'age_group', 'task_group', 'logo', 'main_image'];
 
     public function eventOccurences(){
         return $this->belongsToMany('App\EventOccurrence');
