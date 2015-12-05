da017a7d1f4e8e9282623832337499c3d8181df7
VoxWave <victor_bankowski@hotmail.com>
2015-12-05 22:25:44 +0200

    trying to fix a sql error when retrieving activities from POF backend

--- app/Activity.php
+++ app/Activity.php
@@ -11,20 +11,20 @@ class Activity extends Model
      *
      * @var array
      */
-    protected $fillable = ['name', 'guid', 'age_group', 'task_group'];
-    
+    protected $fillable = ['name', 'guid', 'age_group', 'task_group', 'logo'];
+
     public function eventOccurences(){
         return $this->belongsToMany('App\EventOccurrence');
     }
-    
+
     public function eventPatterns(){
         return $this->belongsToMany('App\EventPattern');
     }
-    
+
     public function users(){
         return $this->belongsToMany('App\User');
     }
-    
+
     public function comments(){
         return $this->morphMany('App\Comment','imageable');
     }
