064cc026ba6c247023097a9f0e0b9fa273b76456
Antti Rantapelkonen <rantaana@gmail.com>
2015-12-09 19:01:42 +0200

    jatkettu maikkelin aloittamaa share-viewiä, ei suinkaan vielä valmis

--- resources/views/activityShare.blade.php
+++ resources/views/activityShare.blade.php
@@ -1,3 +1,7 @@
+
+@section('content')
+@section('title', 'Aktiviteetti')
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -5,20 +9,24 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    
+
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <img src="http://toiminta.partio.fi/sites/partio.fi/files/uploads/partion-logo/partion_logo_web_gif.gif" align="right"></img>
         <div class="container">
 
-    <h1>{{ $activity->name }}</h1>
-    <hr>
-    <p>{{$user->firstname . ' ' . $user->lastname}} on ansainnut seuraavan suoritteen:</p>
-    
-    
-    
-</div>
+            <h1>{{ $activity->name }}</h1>
+            <hr>
+            <p>{{$user->firstname . ' ' . $user->lastname}} on ansainnut seuraavan suoritteen:</p>
+            <p>{{ $activity->name }}</p>
+            <p>{{ $singleActArray['content']}}</p>
+            <img src="{{$singleActArray['logo']}}"></img>
+
+
+
+        </div>
     </body>
 </html>
